5fdeb4c35a44e441395bde8feaf69b3fd459226e
Rudy De Busscher <rdebusscher@gmail.com>
2018-11-22 16:49:57 +0100

    Remove delayClose() and define terminal = true as default. (API, TCK, SPEC updates). Fixes #40

--- tck/src/main/java/org/eclipse/microprofile/lra/tck/participant/api/ActivityController.java
+++ tck/src/main/java/org/eclipse/microprofile/lra/tck/participant/api/ActivityController.java
@@ -249,7 +249,8 @@ public class ActivityController {
 
     @PUT
     @Path(ActivityController.ACCEPT_WORK)
-    @LRA(LRA.Type.REQUIRED)
+    @LRA(value = LRA.Type.REQUIRED, terminal = false)
+    // TODO Verify, this seems not used by any of the TCK tests
     public Response acceptWork(
             @HeaderParam(LRA_HTTP_RECOVERY_HEADER) String rcvId,
             @HeaderParam(LRA_HTTP_HEADER) String lraId) {
@@ -267,7 +268,8 @@ public class ActivityController {
 
     @PUT
     @Path("/supports")
-    @LRA(LRA.Type.SUPPORTS)
+    @LRA(value = LRA.Type.SUPPORTS, terminal = false)
+    // TODO Verify, this seems not used by any of the TCK tests
     public Response supportsLRACall(@HeaderParam(LRA_HTTP_HEADER) String lraId) {
         assertHeaderPresent(lraId);
 
@@ -304,7 +306,7 @@ public class ActivityController {
 
     @PUT
     @Path("/work")
-    @LRA(LRA.Type.REQUIRED)
+    @LRA(value = LRA.Type.REQUIRED, terminal = false)
     public Response activityWithLRA(@HeaderParam(LRA_HTTP_RECOVERY_HEADER) String rcvId,
                                     @HeaderParam(LRA_HTTP_HEADER) String lraId) {
         assertHeaderPresent(lraId);
@@ -337,7 +339,7 @@ public class ActivityController {
 
     @PUT
     @Path("/nestedActivity")
-    @LRA(LRA.Type.MANDATORY)
+    @LRA(value = LRA.Type.MANDATORY, terminal = false)
     @NestedLRA
     public Response nestedActivity(@HeaderParam(LRA_HTTP_RECOVERY_HEADER) String rcvId,
                                    @HeaderParam(LRA_HTTP_HEADER) String nestedLRAId) {
@@ -353,7 +355,7 @@ public class ActivityController {
 
     @PUT
     @Path("/multiLevelNestedActivity")
-    @LRA(LRA.Type.MANDATORY)
+    @LRA(value = LRA.Type.MANDATORY, terminal = false)
     public Response multiLevelNestedActivity(
             @HeaderParam(LRA_HTTP_RECOVERY_HEADER) String rcvId,
             @HeaderParam(LRA_HTTP_HEADER) String nestedLRAId,
@@ -453,7 +455,7 @@ public class ActivityController {
     @Path("/timeLimit")
     @Produces(MediaType.APPLICATION_JSON)
     @TimeLimit(limit = 100, unit = TimeUnit.MILLISECONDS)
-    @LRA(value = LRA.Type.REQUIRED)
+    @LRA(value = LRA.Type.REQUIRED, terminal = false)
     public Response timeLimit(@HeaderParam(LRA_HTTP_HEADER) String lraId) {
         assertHeaderPresent(lraId);
 
@@ -472,7 +474,7 @@ public class ActivityController {
     @Path("/renewTimeLimit")
     @Produces(MediaType.APPLICATION_JSON)
     @TimeLimit(limit = 100, unit = TimeUnit.MILLISECONDS)
-    @LRA(value = LRA.Type.REQUIRED)
+    @LRA(value = LRA.Type.REQUIRED, terminal = false)
     public Response extendTimeLimit(@HeaderParam(LRA_HTTP_HEADER) String lraId) {
         assertHeaderPresent(lraId);
 

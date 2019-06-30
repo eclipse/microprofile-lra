7f8df78b10a8279c5b4b2cf9d8479bbca24ca9f3
xstefank <xstefank122@gmail.com>
2019-06-30 23:41:50 +0200

    Remove unused import in Status.java
    Signed-off-by: xstefank <xstefank122@gmail.com>

--- api/src/main/java/org/eclipse/microprofile/lra/annotation/Status.java
+++ api/src/main/java/org/eclipse/microprofile/lra/annotation/Status.java
@@ -21,7 +21,6 @@
 package org.eclipse.microprofile.lra.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
